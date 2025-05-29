6fb8108e718ee7a6dc57bcda88cec47462aeb429
Ian Hartwell <ianhartwell86@gmail.com>
2025-05-29 21:39:07 +0100

    Create main.js to add interactivity

---
+++ main.js
@@ -0,0 +1,32 @@
+const profilePhoto = document.getElementById('profile-photo');
+const aboutText = document.getElementById('about-text');
+const projects = document.getElementById('projects');
+const skils = document.getElementById('skills-container');
+
+profilePhoto.onmouseover = function() {
+    this.style.boxShadow = 'none';
+};
+profilePhoto.onmouseout = function() {
+    this.style.boxShadow = '0.1rem 0.1rem 0.25rem 0.1rem rgba(96, 97, 97, 0.25)';
+};
+
+aboutText.onmouseover = function() {
+    this.style.backgroundColor = 'lightgrey';
+};
+aboutText.onmouseout = function() {
+    this.style.backgroundColor = 'rgb(221, 221, 225)';
+};
+
+projects.onmouseover = function() {
+    this.style.backgroundColor = 'lightgrey';
+};
+projects.onmouseout = function() {
+    this.style.backgroundColor = 'rgb(221, 221, 225)';
+};
+
+skills.onmouseover = function() {
+    this.style.backgroundColor = 'lightgrey';
+};
+skills.onmouseout = function() {
+    this.style.backgroundColor = 'rgb(221, 221, 225)';
+};
